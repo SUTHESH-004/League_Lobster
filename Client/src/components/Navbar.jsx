f3a0b947bff86abd81db4869b0e514c1dcb00878
Paulson <paulsonmsc@gmail.com>
2024-10-15 11:30:25 +0530

    started working on Navbar

---
+++ Client/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar () =>{
+    return(
+        <div className="">
+            <div className=""></div>
+            <div className="font">
+                League Lobster
+            </div>
+            <div className=""></div>
+        </div>
+    )
+}
