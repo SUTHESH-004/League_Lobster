97078dac500962be30a4bf4034c4c5beadd868b4
Paulson <paulsonmsc@gmail.com>
2024-10-15 14:15:47 +0530

    branch_test

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,11 +1,13 @@
-const Navbar () =>{
+const Navbar = () =>{
     return(
         <div className="">
             <div className=""></div>
-            <div className="font">
+            <div className="font-mono text-5xl">
                 League Lobster
             </div>
             <div className=""></div>
         </div>
     )
 }
+
+export default Navbar;
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,4 +1,6 @@
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './assets/styles/styles.css'
 
 createRoot(document.getElementById('root')).render(
     <App />
