aa4eb68c82605c3f83ed9e026e6096b339aeeb47
Suthesh pravin.v <sutheshpravin4@gmail.com>
2024-10-21 11:22:38 +0530

    SignUpPage Created

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,12 @@
 import Navbar from "./components/Navbar";
 import SignInPage from "./pages/SignInPage";
+import SignUpPage from "./pages/SignUpPage";
+SignUpPage;
 function App() {
   return (
-<></>
+    <>
+      <SignUpPage></SignUpPage>
+    </>
   );
 }
 
